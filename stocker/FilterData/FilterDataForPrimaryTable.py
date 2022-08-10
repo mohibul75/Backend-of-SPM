3d09905071937f4fe71e63e70f9b3a76003ab4a4
Sabbir <bsse1014@iit.du.ac.bd>
2022-08-11 00:15:47 +0600

    exposed api data of home table and trade statistics

--- stocker/FilterData/FilterDataForPrimaryTable.py
+++ stocker/FilterData/FilterDataForPrimaryTable.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 class object:
     def __init__(self, trading_code, ltp, closep, change, ycp, ):
         self.trading_code = trading_code
@@ -13,27 +16,23 @@ class status:
         self.unchanged = unchanged
 
 
-def get_response():
-    import requests
-    import json
+def get_home_comapny_data():
     arr = []
     response = requests.get(
         "https://www.amarstock.com/LatestPrice/34267d8d73dd?fbclid=IwAR3Nnl2tdnlEuJTOlZgH4yBuQR9ngbSg7y70e_kskcaWqwBfdqSkE7E8-II")
-    #print(len(response.json()))
     for item in response.json():
         obj = object(item['FullName'], item['LTP'], item['Close'], item['Change'], item['YCP'])
-        #print(json.dumps(obj.__dict__))
-        arr.append(json.dumps(obj.__dict__))
-    print(arr)
-    #print(len(arr))
+        arr.append(obj.__dict__)
 
+    return arr
+
+def get_trade_statistics():
     status_response = requests.get(
         "https://www.amarstock.com/Info/DSE")
     status_response_data = status_response.json()
     stat = status(status_response_data['Advance'], status_response_data['Decline'], status_response_data['Unchange'])
-    print(json.dumps(stat.__dict__))
 
-if __name__ == '__main__':
-    get_response()
+    return stat.__dict__
+
 
 
--- stocker/api/views.py
+++ stocker/api/views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from FilterData.FilterDataForPrimaryTable import get_home_comapny_data,get_trade_statistics
+
 @api_view(['GET'])
 def hellow(request):
     return Response(status=status.HTTP_200_OK,
@@ -15,3 +17,15 @@ def hellow(request):
 def spm(request):
     return Response(status=status.HTTP_200_OK,
                     data={"hellow, this is spm"})
+
+
+@api_view(['GET'])
+def home_company_data(request):
+    data = get_home_comapny_data()
+    return Response(status=status.HTTP_200_OK, data=data)
+
+
+@api_view(['GET'])
+def trade_staticis(request):
+    data = get_trade_statistics()
+    return Response(status=status.HTTP_200_OK, data=data)
