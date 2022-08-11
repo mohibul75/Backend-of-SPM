03b4eae9d5575b04a7ad384a870637012a720cbe
bashir <bsse1022@iit.du.ac.bd>
2022-08-12 00:38:16 +0600

    Add percentage

--- stocker/FilterData/FilterDataForPrimaryTable.py
+++ stocker/FilterData/FilterDataForPrimaryTable.py
@@ -1,3 +1,7 @@
+import requests
+import json
+
+
 class object:
     def __init__(self, trading_code, ltp, closep, change, ycp, ):
         self.trading_code = trading_code
@@ -5,6 +9,7 @@ class object:
         self.closep = closep
         self.change = change
         self.ycp = ycp
+
 
 class status:
     def __init__(self, up, down, unchanged, up_percentage, down_percentage, unchanged_percentage):
@@ -15,31 +20,40 @@ class status:
         self.down_percentage = down_percentage
         self.unchanged_percentage = unchanged_percentage
 
+
 def percentage(part, whole):
     percentage = 100 * float(part) / float(whole)
     return percentage
 
-def get_response():
-    import requests
-    import json
+
+def get_company_statistics():
     arr = []
     response = requests.get(
         "https://www.amarstock.com/LatestPrice/34267d8d73dd?fbclid=IwAR3Nnl2tdnlEuJTOlZgH4yBuQR9ngbSg7y70e_kskcaWqwBfdqSkE7E8-II")
-    #print(len(response.json()))
+    # print(len(response.json()))
     for item in response.json():
         obj = object(item['FullName'], item['LTP'], item['Close'], item['Change'], item['YCP'])
-        #print(json.dumps(obj.__dict__))
+        # print(json.dumps(obj.__dict__))
         arr.append(json.dumps(obj.__dict__))
-    print(arr)
-    #print(len(arr))
+    # print(arr)
+    return arr
+    # print(len(arr))
 
+
+def get_trade_statistics():
     status_response = requests.get(
         "https://www.amarstock.com/Info/DSE")
     status_response_data = status_response.json()
 
     total = status_response_data['Advance'] + status_response_data['Decline'] + status_response_data['Unchange']
-    stat = status(status_response_data['Advance'], status_response_data['Decline'], status_response_data['Unchange'], percentage(status_response_data['Advance'], total), percentage(status_response_data['Decline'], total), percentage(status_response_data['Unchange'], total))
-    print(json.dumps(stat.__dict__))
+    stat = status(status_response_data['Advance'], status_response_data['Decline'], status_response_data['Unchange'],
+                  percentage(status_response_data['Advance'], total),
+                  percentage(status_response_data['Decline'], total),
+                  percentage(status_response_data['Unchange'], total))
+    # print(json.dumps(stat.__dict__))
+    return json.dumps(stat.__dict__)
+
 
 if __name__ == '__main__':
-    get_response()
+    print(get_company_statistics())
+    print(get_trade_statistics())
