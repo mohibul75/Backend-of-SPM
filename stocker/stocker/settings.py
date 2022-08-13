3a337f95b40a1e6538e379dd0948f3cfd63653fd
Mohibul Alam <43554251+purbo75@users.noreply.github.com>
2022-08-13 21:11:11 +0600

    Update settings.py

--- stocker/stocker/settings.py
+++ stocker/stocker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-bn4b1m2ro(=5kx)4@ks-dx(_!#f=c9kn)w*ld2clu&0tcnwaq0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HOST_IP'), '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
