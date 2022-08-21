6e97fdfc301fbca33a9ae1d16c9a5853fdc98aa8
bashir <bsse1022@iit.du.ac.bd>
2022-08-21 10:16:15 +0600

    Add seven Technical Indicator to the api

--- stocker/api/views.py
+++ stocker/api/views.py
@@ -1,3 +1,4 @@
+import sys
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,50 +6,49 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from FilterData.FilterDataForPrimaryTable import get_home_comapny_data,get_trade_statistics
+from FilterData.FilterDataForPrimaryTable import *
 from FilterData.json_for_3_enspoints import *
 
 @api_view(['GET'])
 def hellow(request):
-    return Response(status=status.HTTP_200_OK,
-                    data={"hellow, welcome to stocker"})
+    return Response(data={"hellow, welcome to stocker"})
 
 
 @api_view(['GET'])
 def spm(request):
-    return Response(status=status.HTTP_200_OK,
-                    data={"hellow, this is spm"})
+    return Response(data={"hellow, this is spm"})
 
 
 @api_view(['GET'])
 def home_company_data(request):
-    data = get_home_comapny_data()
-    return Response(status=status.HTTP_200_OK, data=data)
+    data = get_company_statistics()
+    return Response(data=data)
 
 
 @api_view(['GET'])
 def trade_staticis(request):
     data = get_trade_statistics()
-    return Response(status=status.HTTP_200_OK, data=data)
-
-@api_view(['GET'])
-def technical_indicators_staticis(request):
-    data = get_technical_indicators_statistics()
     return Response(data=data)
+
 
 @api_view(['GET'])
 def cat_todays_value(request):
     data = todays_value()
-    return Response(status=status.HTTP_200_OK, data=data)
+    return Response(data=data)
 
 
 @api_view(['GET'])
 def cat_compare(request):
     data = compare()
-    return Response(status=status.HTTP_200_OK, data=data)
+    return Response(data=data)
 
 
 @api_view(['GET'])
 def cat_gainer_loser(request):
     data = gainer_loser()
-    return Response(status=status.HTTP_200_OK, data=data)
+    return Response(data=data)
+
+@api_view(['GET'])
+def technical_indicators_staticis(request):
+    data = get_technical_indicators_statistics()
+    return Response(data=data)
