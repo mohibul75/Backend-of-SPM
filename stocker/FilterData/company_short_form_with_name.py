13d9f3ef2d6de21d59ac13a5dc2628c8d0631c34
MdAkramKhanJehad <bsse1007@iit.du.ac.bd>
2022-08-30 18:49:04 +0600

    added 2 stage getting short form, need scrap

--- stocker/FilterData/company_short_form_with_name.py
+++ stocker/FilterData/company_short_form_with_name.py
@@ -1,9 +1,10 @@
 import requests
-
+from bs4 import BeautifulSoup as bs
+import urllib.request as ureq
 
 def company_short_form():
     data = []
-    data_dict = {}
+    
     url = "https://www.amarstock.com/LatestPrice/34267d8d73dd"
     
     response = requests.get(url)
@@ -11,9 +12,33 @@ def company_short_form():
     print(len(data))
     
     for i in range(len(data)):
+        data_dict = {}
+        data_dict['n'] = i
         
-        print(data[i]["Scrip"])
-        print(data[i]["FullName"])
+        data_dict["Short_name"] = data[i]["Scrip"]
         
+        if len(data[i]["FullName"]) < 1:
+        
+            url2 = "https://www.amarstock.com/data/1258dca00155/" + data_dict["Short_name"]
+            response2 = requests.get(url2)
+            print(i, " ", response2.status_code)
+            
+            if response2.status_code == 200:
+                data2 = response2.json()
+                data_dict["Full_name"] = data2["FullName"]
+            # else:
+            #     get_url = requests.get(url2)
+            #     get_text = get_url.text
+            #     soup = bs(get_text, "html.parser")
+            #     company = soup.find_all('h1', {'class':'h2 title'})
+            #     print("company", company)
 
+        else:
+            data_dict["Full_name"] = data[i]["FullName"]
+        
+        data.append(data_dict)
+    
+    # print(data)
+    return data
+    
 company_short_form()
