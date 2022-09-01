ed1bbf1dc097e4d8e51569d8cd95a9a64bc68239
ShahriyarHridoy <35798678+ShahriyarHridoy@users.noreply.github.com>
2022-09-01 12:50:16 +0600

    all company list with details in json format
    here clean the code and some error solved

--- stocker/FilterData/StaticData.py
+++ stocker/FilterData/StaticData.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import csv
+import array as arr
 
 def StaticData():
 	#pass
@@ -10,56 +11,18 @@ def StaticData():
 	companyList = len(todos)
 	print(companyList)
 
-	#for x in range(companyList):
-	header = ['CompanyName', 'Date', 'Close']
-	with open('companyData.csv', 'w', encoding='UTF8') as f:
-		writer = csv.writer(f)
-		writer.writerow(header)
-		for x in todos:
-			#companyId = x['Scrip']
-			companyId = "1JANATAMF"
-			print(companyId)
-			print(x['FullName'])
-			print('')
-			print('')
-			response2 = requests.get("https://www.amarstock.com/data/1258dca00155/"+ companyId +"?fbclid=IwAR12z3-tzoyvn6Fc3Klh1P1U61pPB32RH9ZLD4bWHiWdS2TWWhuy6Thk7-Q")
-			todos2 = json.loads(response2.text)
+	count = 0
+	all_json_list = []
 
-			#for y in todos2:
-			#print(x)
-			#print(todos2)
-				
-			#print(x['FullName'])
-			date= todos2['LastUpdate'].split()
-			#print(date[0])
-			#print(todos2['YCP'])
-			#print(todos2['OpenPrice'])
-			#print(todos2['DayRange'])
-			#print(todos2['MarketCap'])
-			#print(todos2['Volume'])
-			#print(todos2['Week52Range'])
-			#print(todos2['ListingYear'])
-			#print(todos2['LastAGMHeld'])
-			#print(todos2['ListingYear'])
-			#print(todos2['MarketCategory'])
-			#print(todos2['Rating'])
-			#print(todos2['TotalSecurities'])
-			#print(todos2['PaidUpCap'])
-			#print(todos2['Scrip'])
-			#print(todos2['ShareHoldingPercentage2'])
-			#print(todos2['AuthorizedCap'])
-			#print(todos2['Q1Eps'])
-			#print(todos2['Q2Eps'])
-			#print(todos2['Q3Eps'])
-			#print(todos2['Q4Eps'])
+	for x in todos:
+		companyId = x['Scrip']
 			
+		response2 = requests.get("https://www.amarstock.com/data/1258dca00155/"+ companyId +"?fbclid=IwAR12z3-tzoyvn6Fc3Klh1P1U61pPB32RH9ZLD4bWHiWdS2TWWhuy6Thk7-Q")
+		todos2 = json.loads(response2.text)
 
-				#date= todos2['DateString'].split()
+		date= todos2['LastUpdate'].split()
 
-			data=[x['FullName'], date[0], todos2['YCP'], todos2['OpenPrice'], todos2['DayRange'], todos2['MarketCap'], todos2['Volume'], todos2['Week52Range'], todos2['ListingYear'], todos2['LastAGMHeld'], todos2['ListingYear'], todos2['MarketCategory'], todos2['Rating'], todos2['TotalSecurities'], todos2['PaidUpCap'], todos2['Scrip'], todos2['ShareHoldingPercentage2'], todos2['AuthorizedCap'], todos2['Q1Eps'], todos2['Q2Eps'], todos2['Q3Eps'], todos2['Q4Eps'],]
-			writer.writerow(data)
-
-			jdata= {
+		jdata = {
 
 				"FullName": x['FullName'],
 				"LastUpdate": date[0],
@@ -89,17 +52,14 @@ def StaticData():
 				"Total_Shares": todos2['TotalSecurities'],
 				"Paid_up_CAP": todos2['PaidUpCap'],
 
-			}
-			return json.dumps(jdata)
+		}
+		
+		all_json_list.append(jdata)
+		# count= count+1
+
+		# if count>10:
+		# 	break
+			
+	return json.dumps(all_json_list)
 			
 print(StaticData())
-
-			#break
-			#break
-
-		#print(todos[0])
-		#if (x['Scrip'] == '1JANATAMF'):
-		#	print(x)
-
-	#
-	#print(todos2[1])
